90b5eee626189f5d60a132e37a4e7baac692f5a9
asa1615 <asc.s.2127@gmail.com>
2024-03-16 08:22:36 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/example/hello/sample/HelloSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSampleApplication.class, args);
+	}
+
+}
