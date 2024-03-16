6ee42557250cbc5b3d49a5cc81adce2a71dea54d
asa1615 <asc.s.2127@gmail.com>
2024-03-17 05:18:57 +0900

    DELETE処理を追加

--- src/main/java/com/example/hello/sample/HelloControllar.java
+++ src/main/java/com/example/hello/sample/HelloControllar.java
@@ -1,13 +1,15 @@
 package com.example.hello.sample;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloControllar {
     @GetMapping("/hello")
-    public  String hello(){
-        return "hello world";
-    }
+    public String hello(){return "hello world";}
+
+    @DeleteMapping("/hello")
+    public String deletehello(){return "delete hello";}
 
 }
